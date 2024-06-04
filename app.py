b2e31722e798a616e6d5a86a706b2d676c32ecd0
Jai Raj kumar <87121378+JaiRaj22@users.noreply.github.com>
2024-06-04 18:07:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def get_ip():
     user_ip = request.remote_addr
-    return f'Your IP address is: {user_ip}'
+    return f'Your IP address is: {user_ip}. Next time, dont click on random links over the internet. You should be glad i am not logging this.'
 
 if __name__ == '__main__':
     app.run()
