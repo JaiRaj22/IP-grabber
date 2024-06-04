02f6b245520b7d8797dcd09941805a5734d3f548
Jai Raj kumar <87121378+JaiRaj22@users.noreply.github.com>
2024-06-04 18:51:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes and methods
 
 @app.route('/', methods=['GET'])
 def get_ip():
